ff3e8a97e2ac66374c02f90c1579a14de17725c5
MikhailAndreev <mikha.andreev@gmail.com>
2014-04-20 19:13:33 -0400

    Starting data extraction

---
+++ EC512DataScraper/src/control/ControlLoop.java
@@ -0,0 +1,48 @@
+package control;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import util.Param;
+import web.Client;
+
+public class ControlLoop {
+	public static void main(String[] args){
+		try{
+			System.out.println("Start Data Scraper...");
+			
+			System.out.print("Loading file...");
+			BufferedReader br = new BufferedReader(new FileReader(Param.FILE_PATH));
+			System.out.println("\tDone.");
+			
+			System.out.print("Breaking down file into items...");
+			ArrayList<ArrayList<String>> raw_text = new ArrayList<ArrayList<String>>();
+			ArrayList<String> item = new ArrayList<String>();
+			String line;
+			while((line=br.readLine()) != null){
+				if(!line.equals("	"))
+					item.add(line);
+				else{
+					raw_text.add(item);
+					item = new ArrayList<String>();
+				}
+			}
+			System.out.println("\tDone.");
+	
+			System.out.print("Parse Items...");
+			
+			System.out.println("\tDone.");
+			
+			System.out.println("Operations Complete.");
+		} catch (FileNotFoundException e1){
+			System.out.println(String.format("File %s could not be found.", Param.FILE_PATH));
+			System.exit(-1);
+		} catch (IOException e2){
+			System.out.println(String.format("Parsing File %s caused an IOException.", Param.FILE_PATH));
+			System.exit(-1);
+		}
+	}
+}
---
+++ EC512DataScraper/src/util/Param.java
@@ -0,0 +1,9 @@
+package util;
+
+public class Param {
+	public final static String START_URL = "http://www.digikey.com";
+	public final static String SEARCH = "resistor";
+	public final static String SEARCH_BAR_ID = "search-input";
+	public final static String SEARCH_BUTTON_ID = "btnSearch";
+	public final static String FILE_PATH = "data.txt";
+}
