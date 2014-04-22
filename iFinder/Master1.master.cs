4732a5ba9f309c5795cbdbcc210f90c49e56fb50
henryluo <henryluo917@gmail.com>
2014-04-22 15:19:51 -0700

    Added ability to dynamically create visual search filters (controls).

--- iFinder/Master1.master.cs
+++ iFinder/Master1.master.cs
@@ -9,6 +9,26 @@ public partial class Master1 : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        Label filterHeader = new Label();
 
+        filterHeader.Attributes.CssStyle.Add("font-size","100%");
+        filterHeader.Attributes.CssStyle.Add("font-weight", "bold");
+        filterHeader.Attributes.CssStyle.Add("color", "#FF9900");
+        filterHeader.Text = "Label" + "1";
+        filterHeader.ID = "Label" + "1";
+        PlaceHolder1.Controls.Add(filterHeader);
+        PlaceHolder1.Controls.Add(new LiteralControl("<br />"));
+
+        CheckBoxList checkList = new CheckBoxList();
+
+        checkList.Attributes.CssStyle.Add("margin", "0px 0px 6px 0px");
+        checkList.Attributes.CssStyle.Add("padding", "0px 0px 6px 0px");
+        checkList.Attributes.CssStyle.Add("list-style-type", "none");
+        checkList.Attributes.CssStyle.Add("list-style-position", "outside");
+        checkList.Attributes.CssStyle.Add("font-size", "100%");
+        checkList.ID = "Checklist" + "1";
+        checkList.Items.Add("Hello");
+        PlaceHolder1.Controls.Add(checkList);
+        PlaceHolder1.Controls.Add(new LiteralControl("<br />"));
     }
 }
