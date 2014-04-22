f82eb4271de214e74d530231af498a69facdfd3d
MikhailAndreev <mikha.andreev@gmail.com>
2014-04-22 16:42:40 -0400

    Fixed up database creation, working on search function

--- iFinder/Default.aspx.cs
+++ iFinder/Default.aspx.cs
@@ -10,51 +10,74 @@ using System.Text;
 
 public partial class _Default : System.Web.UI.Page
 {
+
+    private String connectionString = "Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\Database2.mdf;Integrated Security=True";
+    private bool sql_defined = false;
+    private bool rewrite_table = true;
+    private String tableName;
+    private String data_xml_path = "data.xml";
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        DataSet ds = new DataSet();
-        
-        ds.ReadXml(Server.MapPath("data.xml"));
-        String type = "", name = "";
-        foreach (DataRow row in ds.Tables[0].Rows)
+        if (!sql_defined || rewrite_table)
         {
-            type = row["ProductType"].ToString();
-            name = row["ProductName"].ToString();
-        }
-        StringBuilder query = new StringBuilder(), table_name = new StringBuilder(), delete_table_query = new StringBuilder();
-        String connectionString = "Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\Database2.mdf;Integrated Security=True";
+            try
+            {
+                DataSet ds = new DataSet();
+
+                ds.ReadXml(Server.MapPath(data_xml_path));
+                String type = "", name = "";
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    type = row["ProductType"].ToString();
+                    name = row["ProductName"].ToString();
+                }
+
+                StringBuilder table_name = new StringBuilder();
+                table_name.Append(type);
+                table_name.Append("_");
+                table_name.Append(name);
+                table_name.Append("_table");
+
+                tableName = table_name.ToString();
+
+                deleteTable(tableName);
+                List<String> cols = createTable(tableName, ds);
+                insertDataIntoTable(tableName, ds, cols);
+                sql_defined = true;
+            }
+            catch (Exception ex)
+            {
+                debug.Text = "Error in loading database";
+            }
+        }   
+    }
+
+    private void deleteTable(String tableName)
+    {
         SqlConnection sqlConn = new SqlConnection(connectionString);
-        SqlCommand sqlQuery;
-        SqlDataReader reader;
-        
-
-        table_name.Append(type);
-        table_name.Append("_");
-        table_name.Append(name);
-        table_name.Append("_table");
-
-        /*** Need to remove the table if it already exists ***/
+        StringBuilder delete_table_query = new StringBuilder();
         sqlConn.Open();
         delete_table_query.Append("IF OBJECT_ID ( '");
-        delete_table_query.Append(table_name.ToString());
+        delete_table_query.Append(tableName.ToString());
         delete_table_query.Append("', 'U') IS NOT NULL DROP TABLE ");
-        delete_table_query.Append(table_name.ToString());
-        sqlQuery = new SqlCommand(delete_table_query.ToString(), sqlConn);
-        reader = sqlQuery.ExecuteReader();
+        delete_table_query.Append(tableName.ToString());
+        SqlCommand sqlQuery = new SqlCommand(delete_table_query.ToString(), sqlConn);
+        sqlQuery.ExecuteNonQuery();
         sqlConn.Close();
-        /*** Table removed ***/
-        
+    }
 
-        /*** Create the table ***/
+    private List<String> createTable(String tableName, DataSet data)
+    {
+        SqlConnection sqlConn = new SqlConnection(connectionString);
+        StringBuilder query = new StringBuilder();
         query.Append("CREATE TABLE ");
-        query.Append(type);
-        query.Append("_");
-        query.Append(name);
-        query.Append("_table ( ");
+        query.Append(tableName);
+        query.Append(" ( ");
 
         List<String> cols = new List<String>();
-        
-        foreach (DataColumn col in ds.Tables[2].Columns)
+
+        foreach (DataColumn col in data.Tables[2].Columns)
         {
             query.Append(col.ColumnName);
             cols.Add(col.ColumnName);
@@ -65,54 +88,20 @@ public partial class _Default : System.Web.UI.Page
         query.Append(")");
 
         sqlConn.Open();
-        sqlQuery = new SqlCommand(query.ToString(), sqlConn);
-        reader = sqlQuery.ExecuteReader();
+        SqlCommand sqlQuery = new SqlCommand(query.ToString(), sqlConn);
+        sqlQuery.ExecuteNonQuery();
         sqlConn.Close();
-        /*** Table Created ***/
+        return cols;
+    }
 
-
-        /*** Insert Data Values ***/
-        
-        foreach (DataRow row in ds.Tables[2].Rows)
+    private void insertDataIntoTable(String tableName, DataSet data, List<String> cols)
+    {
+        foreach (DataRow row in data.Tables[2].Rows)
         {
+            SqlConnection sqlConn = new SqlConnection(connectionString);
             StringBuilder insert_query = new StringBuilder();
-          //  insert_query.Append("INSERT INTO ");
-          //  insert_query.Append(table_name);
-          //  insert_query.Append(" (ID) VALUES (HEllo);");
-          //  insert_query.Append("'(");
-          //  insert_query.Append("ID) VALUES (HEllo);");
-         /*   foreach (String str in cols)
-            {
-                insert_query.Append(str);
-                insert_query.Append(", ");
-            }
-            insert_query.Length -= 2;
-            insert_query.Append(") VALUES (");
-            foreach (String str in cols)
-            {
-              //  insert_query.Append("@");
-              //  insert_query.Append(str);
-               // insert_query.Append(", ");
-                insert_query.Append(row[str].ToString());
-                insert_query.Append(", ");
-            } 
-
-            insert_query.Length -= 2; 
-            insert_query.Append(")"); */
-          //  sqlConn.Open();
-          //  sqlQuery = new SqlCommand(insert_query.ToString(), sqlConn); 
-          /*  foreach (String str in cols)
-            {
-                StringBuilder parameter_name = new StringBuilder();
-                parameter_name.Append("@");
-                parameter_name.Append(str);
-                
-            }*/
-         //   sqlQuery.ExecuteNonQuery();
-           // reader = sqlQuery.ExecuteReader();
-        //    sqlConn.Close();
             insert_query.Append("INSERT INTO ");
-            insert_query.Append(table_name);
+            insert_query.Append(tableName);
             insert_query.Append(" VALUES (");
             foreach (String str in cols)
             {
@@ -123,7 +112,7 @@ public partial class _Default : System.Web.UI.Page
             insert_query.Length -= 2;
             insert_query.Append(");");
             sqlConn.Open();
-            sqlQuery = new SqlCommand(insert_query.ToString(), sqlConn);
+            SqlCommand sqlQuery = new SqlCommand(insert_query.ToString(), sqlConn);
             foreach (String str in cols)
             {
                 StringBuilder parameter_name = new StringBuilder();
@@ -134,8 +123,10 @@ public partial class _Default : System.Web.UI.Page
             sqlQuery.ExecuteNonQuery();
             sqlConn.Close();
         }
-        /*** Done Inserting ***/
+    }
 
-        debug.Text = "database loaded";
+    private void searchQuery()
+    {
+
     }
 }
