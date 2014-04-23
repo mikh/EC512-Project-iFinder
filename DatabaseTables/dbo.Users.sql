57966c79c19004cfac2e07185f41fa126454860a
Andy Mo <andy2m11@gmail.com>
2014-04-23 15:52:28 -0400

    Save db definition files for now.

---
+++ DatabaseTables/dbo.Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [UserName] NVARCHAR (MAX) NOT NULL,
+    [Password] NVARCHAR (MAX) NOT NULL,
+    [Role]     INT            DEFAULT ((0)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ DatabaseTables/dbo.electronics_resistor_table.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[electronics_resistor_table] (
+    [ID]               NVARCHAR (MAX) NULL,
+    [manufacturer]     NVARCHAR (MAX) NULL,
+    [quantity]         NVARCHAR (MAX) NULL,
+    [availability]     NVARCHAR (MAX) NULL,
+    [price]            NVARCHAR (MAX) NULL,
+    [minimum_quantity] NVARCHAR (MAX) NULL,
+    [packaging]        NVARCHAR (MAX) NULL,
+    [series]           NVARCHAR (MAX) NULL,
+    [resistance_ohms]  NVARCHAR (MAX) NULL,
+    [tolerance]        NVARCHAR (MAX) NULL,
+    [power_watts]      NVARCHAR (MAX) NULL,
+    [items_Id]         NVARCHAR (MAX) NULL
+);
+
